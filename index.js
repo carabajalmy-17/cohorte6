a3c2065460b48f988686f602ad5f00fc411d9370
malena carabajal <carabajalmalena65@gmail.com>
2026-08-23 22:36:26 -0300

    modificaciones

---
+++ index.js
@@ -0,0 +1,5 @@
+const nombre = process.argv[2] || "Estudiante";
+
+console.log("nombre del estudiante: ", nombre);
+console.log("Version de Node.js: ", process.version);
+console.log("Plataforma: ", process.platform);
